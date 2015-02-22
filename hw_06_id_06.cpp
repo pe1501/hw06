096e3405d35d8fe174c640f4be994adc73115741
CarlosAlam <alambataco@gmail.com>
2015-02-22 17:06:16 -0600

    Update hw_06_id_06.cpp

--- hw_06_id_06.cpp
+++ hw_06_id_06.cpp
@@ -1,19 +1,26 @@
-// Author:
-// Date:
-// Version:
+// Author:Carlos Alam Martinez Castillo
+// Date:22/02/2015
+// Version:conversion de temperatura de C° a F°
 
 /*
   Homework DD
   this assignment ...
 */
 
+
+
 #include <iostream>
+#include <conio.h>
+using namespace std;
 
 int main ()
 {
-  // TODO your code here!!!!
-
-  std::cout<<"Hello world!!" <<std::endl;
-
-  return 0;
+	double a;
+	cout<<" escriba la temperatura C° para convertir a F°"<<endl;
+	cin>>a;
+	cout<<(a*9/5)+32<<endl;
+	
+	
+	system ("pause");
+	return 0;
 }
