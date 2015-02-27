27a6f825d904bfde589f340121af7e8ffec6e7df
Haruso <upci.pe1501@gmail.com>
2015-02-26 23:07:22 -0600

    Tarea resuelta
    Solución a ala tarea, versión compilable, validada, sin errores y
    cumpliendo las especificaciones planteadas. Versión 1.0

--- hw_06_id_27.cpp
+++ hw_06_id_27.cpp
@@ -1,19 +1,26 @@
-// Author:
-// Date:
-// Version:
-
 /*
-  Homework DD
-  this assignment ...
+Author:   Haruso Violeta
+Date:     today
+Version:  1.0
+
+  Homework 06
+  Este programa imprime en pantalla el codigo ASCII
+	
 */
 
 #include <iostream>
 
-int main ()
+int main(int argc, char **argv)
 {
-  // TODO your code here!!!!
+	
+	std::cout<<"\nEste programa imprime en pantalla el codigo ASCII \n\n";
 
-  std::cout<<"Hello world!!" <<std::endl;
-
-  return 0;
+	for(int i = 0; i < 16; i ++)
+	{
+		for(int j = 0; j < 8; j ++)
+			std::cout << (char) (i + j* 16) << "\t";
+			
+		std::cout << "\n\n";
+	}	
+	return 0;
 }
